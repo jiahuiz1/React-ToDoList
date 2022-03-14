f2b2ce81f056879901bda5bb8e94eb881c9359f3
jiahuiz1 <jiahuiz@ksu.edu>
2022-03-13 21:46:53 -0700

    to do list with edit

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react'
-import {useState} from 'react-redux'
+import {useState} from 'react';
 import './App.css';
 
+
+function changeList(content){
+  return(
+    <li>
+      <span>{content}</span>
+      <button>edit</button>
+      <button>delete</button>
+    </li>
+  )
+}
+
 function App() {
+  const [list, setList] = useState([]);
+  const [inputValue, setInput] = useState(0);
+  console.log(inputValue);
+
+  const addItem = () => {
+    setInput(inputValue+1);
+  }
+
   return (
     <div className="ToDoList">
-      <input></input>
-      <button>Edit</button>
+      <header>
+        <h1>To do list</h1>
+      </header>
+        <form>
+          {/* <input onChange={(e) => setInput(e.target.value)}></input> */}
+          <button onClick={addItem}>Add</button>
+        </form>
+        <p>{inputValue}</p>
     </div>
   );
 }
